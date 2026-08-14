e9fc04d42866f286090d48a8ed31ecb41a5d7b2d
tom1022 <tom1022.v2@gmail.com>
2026-08-14 22:31:20 +0900

    feat(announcements-list): 限定数を指定してお知らせを表示する機能を追加

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -43,7 +43,10 @@ export default async function Page() {
               <h2 className="mb-4 border-b border-gray-200 pb-2 text-2xl font-bold">
                 お知らせ
               </h2>
-              <AnnouncementsList announcements={content.announcements} />
+              <AnnouncementsList
+                announcements={content.announcements}
+                limit={5}
+              />
             </section>
 
             {content.topics.length > 0 && (
